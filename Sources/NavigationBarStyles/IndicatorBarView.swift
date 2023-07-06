04bf164a727444e352aae8fa06510576c91002d6
Hassan dad khan <39277328+Hassandadkhanit@users.noreply.github.com>
2023-07-06 17:47:06 +0500

    Update IndicatorBarView.swift
    Removing error 'Initializer for conditional binding must have Optional type, not 'CGFloat'

--- Sources/NavigationBarStyles/IndicatorBarView.swift
+++ Sources/NavigationBarStyles/IndicatorBarView.swift
@@ -16,7 +16,7 @@ internal struct IndicatorBarView<Indicator: View>: View {
     var body: some View {
         HStack {
             let totalItemWidth = (settings.width - (style.tabItemSpacing * CGFloat(dataStore.itemsCount - 1)))
-            let navBarItemWidth = totalItemWidth / CGFloat(dataStore.itemsCount)
+            let navBarItemWidth: CGFloat? = totalItemWidth / CGFloat(dataStore.itemsCount)
             if let width = navBarItemWidth, width > 0, width <= settings.width {
                 let x = -settings.contentOffset / CGFloat(dataStore.itemsCount) + width / 2
 
